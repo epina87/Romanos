a5a56ae81d7f33006bf43bedabe411ab77c6d284
Enric Pina <epina87@gmail.com>
2022-07-20 08:25:39 +0200

    de romano a entero

--- first_part.py
+++ first_part.py
@@ -8,28 +8,52 @@
 class RomanNumberError(Exception):
     pass
 
-numero_romano = (
-    (1000, 'M'), (500, 'D'), (100, 'C'), (50, 'D'), (10, 'X'), (5, 'V'), (1, 'I')
+numero_romano = {
+    1000: 'M', 500: 'D', 100: 'C', 50: 'D', 10: 'X', 5: 'V', 1: 'I'
+}
+
+algoritmo_romano = (
+    {1: 'M'},
+    {1: 'C', 5: 'D', 10: 'M'},
+    {1: 'X', 5: 'L', 10: 'C'},
+    {1: 'I', 5: 'V', 10: 'X'},
 )
 
-centenas = (
-    (100, 'C'), (200, 'CC'), (300, 'CCC'),
-    (400, 'CD'), (500, 'D'), (600, 'DC'),
-    (700, 'DCC'), (800, 'DCCC'), (900, 'CM')
-)
 
-decenas = (
-    (10, 'X'), (20, 'XX'), (30, 'XXX'),
-    (40, 'XL'), (50, 'L'), (60, 'LX'),
-    (70, 'LXX'), (80, 'LXXX'), (90, 'XC')
-)
+componentes = {
+    1000: 'M', 2000: 'MM', 3000: 'MMM', 
+    100: 'C', 200: 'CC', 300: 'CCC',
+    400: 'CD', 500: 'D', 600: 'DC',
+    700: 'DCC', 800: 'DCCC', 900: 'CM',
+    10: 'X', 20: 'XX', 30: 'XXX',
+    40: 'XL', 50: 'L', 60: 'LX',
+    70: 'LXX', 80: 'LXXX', 90: 'XC',
+    1: 'I', 2: 'II', 3: 'III',
+    4: 'IL', 5: 'V', 6: 'VI',
+    7: 'VII', 8: 'VIII', 9: 'IX'
+}    
 
-unidades = (
-    (1, 'I'), (2, 'II'), (3, 'III'),
-    (4, 'IL'), (5, 'V'), (6, 'VI'),
-    (7, 'VII'), (8, 'VIII'), (9, 'IX')
-)
 
+def entero_a_romano_2(numero):
+    numero = "{:0>4d}".format(numero)
+    digitos = list(numero)
+
+    romano = ''
+
+    for tipo_unidad, digito in enumerate(digitos):
+        digito = int(digito)
+        tupla_activa = algoritmo_romano[tipo_unidad]
+
+        if digito < 4:
+            romano += tupla_activa[1] * digito
+        elif digito == 4:
+            romano += tupla_activa[1] + tupla_activa[5]
+        elif int(digitos[1]) < 9:
+            romano += tupla_activa[5] + tupla_activa[1] * (digito - 5)
+        else:
+            romano += tupla_activa[1] + tupla_activa[10]
+
+    return romano
 
 
 def entero_a_romano(numero):
@@ -43,14 +67,60 @@ def entero_a_romano(numero):
     numero = "{:0>4d}".format(numero)
     digitos = list(numero)
 
-    ix = 0
     longitud = len(digitos)
-    for n in numero:
+    romano = ''
+    for ix in range(len(numero)):
         longitud -= 1
         digitos[ix] = digitos[ix] + "0" * longitud
-        ix += 1
+        romano += componentes.get(int(digitos[ix]), "")
 
-    return digitos
+    return romano
 
 
-entero_a_romano(336)
+#------------------------------------------------Entero Romano    
+
+def romano_a_entero(n_romano):
+    dic_romano = conbertir_dic_romano() 
+
+    n_romano2 = n_romano
+    numero_entero = 0
+    posicion_n_romano = 0
+    while posicion_n_romano<len(n_romano):   
+
+        n =  n_romano[posicion_n_romano]
+        valor_entero = dic_romano.get(n,"")  
+        ix=posicion_n_romano + 1
+        contador_posiciones_sumar = 0
+        contador_valores_iguales  = 0
+        while ix<len(n_romano2):
+            valor_siguiente = n_romano[ix]
+            valor_entero_siguiente = dic_romano.get(valor_siguiente,"") 
+            if valor_entero_siguiente > valor_entero:
+                valor_entero = valor_entero_siguiente - valor_entero
+                contador_posiciones_sumar += 1 
+            if valor_entero_siguiente == valor_entero:
+                contador_valores_iguales += 1
+
+            ix += 1 
+
+        if contador_valores_iguales > 3:
+            numero_entero = 0
+            print("El número romano no es correcto")
+            break
+
+        numero_entero += valor_entero        
+        posicion_n_romano += 1 + contador_posiciones_sumar
+    
+    print(numero_entero)           
+
+def conbertir_dic_romano():
+    dic_romano = {}
+    for clave,valor in componentes.items():
+        dic_romano[valor] = clave
+    return dic_romano
+        
+
+
+#1996 - MCMXCVI
+#336  - CCCXXXVI
+romano_a_entero("MCMXCVI")
