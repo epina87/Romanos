05b9d251684080adab1052fa3d40ddd07c9475cc
Enric Pina <epina87@gmail.com>
2022-07-20 09:08:37 +0200

    mejorando controlar letras no válidas

--- f_romano_a_entero.py
+++ f_romano_a_entero.py
@@ -11,7 +11,7 @@ componentes = {
     4: 'IL', 5: 'V', 6: 'VI',
     7: 'VII', 8: 'VIII', 9: 'IX'
 }  
-  
+
 def conbertir_dic_romano():
     dic_romano = {}
     for clave,valor in componentes.items():
@@ -19,6 +19,7 @@ def conbertir_dic_romano():
     return dic_romano
 
 def romano_a_entero(n_romano):
+    error = ""
     dic_romano = conbertir_dic_romano() 
 
     n_romano2 = n_romano
@@ -27,35 +28,46 @@ def romano_a_entero(n_romano):
     while posicion_n_romano<len(n_romano):   
 
         n =  n_romano[posicion_n_romano]
-        valor_entero = dic_romano.get(n,"")  
+        valor_entero = dic_romano.get(n,"error") 
+        if valor_entero == "error":
+            numero_entero = 0
+            error = "Valores Incorrectos"
+            break 
+        
         ix=posicion_n_romano + 1
 
         contador_posiciones_sumar = 0
-        contador_valores_iguales  = 0
+        contador_valores_iguales  = 1
         while ix<len(n_romano2):
             valor_siguiente = n_romano[ix]
-            valor_entero_siguiente = dic_romano.get(valor_siguiente,"") 
-            if valor_entero_siguiente > valor_entero:
+            valor_entero_siguiente = dic_romano.get(valor_siguiente,"error") 
+            if valor_entero_siguiente == "error":
+                error = "Valores Incorrectos"
+                break
+            elif valor_entero_siguiente > valor_entero:
                 valor_entero = valor_entero_siguiente - valor_entero
                 contador_posiciones_sumar += 1 
             elif valor_entero_siguiente == valor_entero:
                 contador_valores_iguales += 1
             else:   
-                break
-            
+                break           
             ix += 1 
 
         if contador_valores_iguales > 3:
             numero_entero = 0
-            print("El número romano no es correcto")
+            error = "El número romano no es correcto"
             break
 
         numero_entero += valor_entero        
         posicion_n_romano += 1 + contador_posiciones_sumar
     
-    return numero_entero
+    return numero_entero,error
+
 
 valor = input("Número romano:")
-valor_romano = romano_a_entero(valor)
-print("Valor entero: ",  valor_romano)
+valor_romano,error = romano_a_entero(valor)
+if error == "":
+    print("Valor entero: ",  valor_romano)
+else:
+    print(error)
         
